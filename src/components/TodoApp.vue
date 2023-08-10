42b9a9d5d7242c1e641f361449555c68a8da4ca3
guidimarco <mark.88g@gmail.com>
2023-08-10 12:44:58 +0200

    Create our first store

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,9 +1,14 @@
 <script setup>
-
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
 </script>
 
 <template>
-
+    <div class="todo-app">
+        <h1>To Do List</h1>
+        <todo-form />
+        <todo-list />
+    </div>
 </template>
 
 <style scoped>
