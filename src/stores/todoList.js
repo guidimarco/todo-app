83e9447b4e65c136f0c04b944b98d92e10e3f24f
guidimarco <mark.88g@gmail.com>
2023-08-10 15:26:27 +0200

    Mutating state with Actions

--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -1,7 +1,18 @@
 import { defineStore } from 'pinia'
 
-export const useTodoListStore = defineStore('todoList', () => {
+export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    todoList: []
-  })
+    todoList: [],
+    id: 0
+  }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteItem(itemId) {
+      this.todoList = this.todoList.filter((obj) => {
+        return obj.id !== itemId
+      })
+    }
+  }
 })
